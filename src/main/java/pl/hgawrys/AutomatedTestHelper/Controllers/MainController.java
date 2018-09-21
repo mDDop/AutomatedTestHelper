84677cabbb8f01455d3deff98466fc50ce18c3c4
mddop <hubertgawrys@gmail.com>
2018-09-21 18:22:16 +0200

    v2

--- src/main/java/pl/hgawrys/AutomatedTestHelper/Controllers/MainController.java
+++ src/main/java/pl/hgawrys/AutomatedTestHelper/Controllers/MainController.java
@@ -1,8 +1,12 @@
 package pl.hgawrys.AutomatedTestHelper.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.hgawrys.AutomatedTestHelper.VariableCreator;
 
 @Controller
 public class MainController {
@@ -10,8 +14,24 @@ public class MainController {
     public void run() {
     }
 
+    @Autowired
+    VariableCreator vCreator;
+
     @GetMapping("/")
     public String index(Model model){
         return "index";
     }
+
+    @PostMapping("/")
+    public String index(Model model,
+                        @RequestParam("dataInput") String dataInput){
+
+        String dataOutput = "kk";
+        System.out.println(dataInput);
+
+        model.addAttribute("dataOutput", vCreator.getVariables(dataInput));
+
+        return "index";
+    }
+
 }
