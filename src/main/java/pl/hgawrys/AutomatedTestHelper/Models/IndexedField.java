0fea11b58e02394bac39a332a20e5fa76f17308e
mddop <hubertgawrys@gmail.com>
2019-01-05 00:27:15 +0100

    v2

--- src/main/java/pl/hgawrys/AutomatedTestHelper/Models/IndexedField.java
+++ src/main/java/pl/hgawrys/AutomatedTestHelper/Models/IndexedField.java
@@ -24,7 +24,7 @@ public class IndexedField {
 
     @Override
     public String toString() {
-        return "IndexedField{" +
+        return "IndexedField {" +
                 "indexNumber=" + indexNumber +
                 ", nameOfField='" + nameOfField + '\'' +
                 '}';
